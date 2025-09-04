b8a9d966b96371c966c76f53f64201124b252f7c
ayidveroo2024 <stop47ay u@gmail.com>
2025-09-05 01:20:16 +0300

    header icons

--- Netflix-clone/src/components/Header/Header.jsx
+++ Netflix-clone/src/components/Header/Header.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Header.css";
 import NetflixLogo from "../../assets/images/NetflixLogo.png";
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 const Header = () => {
   return (
@@ -21,10 +25,10 @@ const Header = () => {
         </div>
         <div className="header_right">
           <ul>
-            {/* <li><SearchIcon/></li>
+            <li><SearchIcon/></li>
             <li><NotificationsNoneIcon/></li>
             <li><AccountBoxIcon/></li>
-            <li><ArrowDropDownIcon/></li> */}
+            <li><ArrowDropDownIcon/></li>
           </ul>
         </div>
       </div>
