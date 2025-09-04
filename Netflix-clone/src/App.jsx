73eeb0da8389c462367727dd01ba3e26e74551f1
ayidveroo2024 <stop47ay u@gmail.com>
2025-09-05 00:48:01 +0300

    header

--- Netflix-clone/src/App.jsx
+++ Netflix-clone/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import "./App.css";
+import Home from "./pages/Home/Home";
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
       <div className="App">
-        <h1>Hello World!</h1>
+        <Home />
       </div>
     </>
   );
