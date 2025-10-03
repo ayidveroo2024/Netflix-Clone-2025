949356ac09e480775be56c6e665b63d91ee076c0
ayidveroo2024 <stop47ay u@gmail.com>
2025-10-03 12:07:30 +0300

    deploy

--- Netflix-clone/src/components/Header/Header.jsx
+++ Netflix-clone/src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Header.css";
 import NetflixLogo from "../../assets/images/NetflixLogo.png";
-import SearchIcon from '@mui/icons-material/Search';
-import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import Link from "@mui/material/Link";
 
 const Header = () => {
   return (
@@ -12,8 +13,9 @@ const Header = () => {
       <div className="header_container">
         <div className="header_left">
           <ul>
-            
-            <li><img src={NetflixLogo} alt="Netflix Logo" width="100" /></li>
+            <li>
+              <img src={NetflixLogo} alt="Netflix Logo" width="100" />
+            </li>
             <li>Netflix</li>
             <li>Home</li>
             <li>TVShows</li>
@@ -25,10 +27,18 @@ const Header = () => {
         </div>
         <div className="header_right">
           <ul>
-            <li><SearchIcon/></li>
-            <li><NotificationsNoneIcon/></li>
-            <li><AccountBoxIcon/></li>
-            <li><ArrowDropDownIcon/></li>
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+              <NotificationsNoneIcon />
+            </li>
+            <li>
+              <AccountBoxIcon />
+            </li>
+            <li>
+              <ArrowDropDownIcon />
+            </li>
           </ul>
         </div>
       </div>
