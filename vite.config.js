d709db08426e318662c9c84b90e4ff1bea27a7ab
ayidveroo2024 <stop47ay u@gmail.com>
2025-10-13 15:10:16 +0300

    updating vit.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Netflix-Clone-Rev", // Add this line to set the base path
+  base: "/Netflix-Clone-Rev/", // Add this line to set the base path
 });
